5a3cfa48cf3ee1b5d434bb6cfaebe645237ade39
Mark Kauffman <markkauffman22@gmail.com>
2019-10-03 05:17:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 // Notes only - this does not work yet ... Ideas are right.
 
 
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft();
-  }
-  else if(e.which === 39) {
-  	moveDodgerRight();
-  } 
-});
-
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '');
@@ -24,7 +15,7 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
  
-  if (left < 380) {
+  if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
 }
