de6abc1a9f3a0cfa24665805e873c3b540fe9ea7
Mark Kauffman <markkauffman22@gmail.com>
2019-10-03 05:11:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function moveDodgerRight() {
   var left = parseInt(leftNumbers, 10);
  
   if (left < 380) {
-    dodger.style.left = `${left a + 1}px`;
+    dodger.style.left = `${left + 1}px`;
   }
 }
 
